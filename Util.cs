79c43611f38d07f5da5379bf338847b4dafdf4ea
Zhuyi Bu <101956702+1144761429@users.noreply.github.com>
2023-10-19 23:30:22 -0600

    Add files via upload

---
+++ Util.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace Leetcode;
+
+public abstract class Util
+{
+    public static string DebugArray<T>(T[] arr)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        foreach (T element in arr)
+        {
+            stringBuilder.Append(element?.ToString() + " ");
+        }
+
+        return stringBuilder.ToString();
+    }
+}

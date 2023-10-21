ad6a88ebd1184226ed66f4a7252c192d8885529d
Zhuyi Bu <101956702+1144761429@users.noreply.github.com>
2023-10-20 23:30:17 -0600

    Add files via upload

---
+++ ArrayUtil.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace Leetcode;
+
+public abstract class ArrayUtil
+{
+    public static string DebugArray<T>(T[] arr)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.Append("[");
+        IEnumerator iterator = arr.GetEnumerator();
+        iterator.MoveNext();
+        foreach (var element in arr)
+        {
+            stringBuilder.Append($"{element?.ToString()}");
+            if (iterator.MoveNext())
+            {
+                stringBuilder.Append($", ");
+            }
+        }
+        stringBuilder.Append("]");
+        return stringBuilder.ToString();
+    }
+
+    
+    public static int[] FindMaxDiffSubArray_Negative(int[] arr)
+    {
+        int smallestValue = int.MaxValue;
+        int difference = 0;
+
+        int tempStartIdx = -1;
+        int startIdx = -1;
+        int endIdx = -1;
+        
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] < smallestValue)
+            {
+                smallestValue = arr[i];
+                tempStartIdx = i;
+            }
+            else if(arr[i] - smallestValue > difference)
+            {
+                difference = arr[i] - smallestValue;
+                startIdx = tempStartIdx;
+                endIdx = i;
+            }
+        }
+
+        return new int[] { startIdx, endIdx };
+    }
+}
---
+++ Q217_ContainsDuplicate.cs
@@ -0,0 +1,33 @@
+﻿namespace Leetcode;
+
+// Array, Hashtable, Sorting
+public class Q217_ContainsDuplicate
+{
+    // Given an integer array nums,
+    // return true if any value appears at least twice in the array,
+    // and return false if every element is distinct.   
+    
+    // Constraints:
+    //
+    // 1 <= nums.length <= 105
+    // -109 <= nums[i] <= 109
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        if (nums.Length == 1)
+        {
+            return false;
+        }
+        
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        foreach (int element in nums)
+        {
+            if (!dict.TryAdd(element, 0))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
